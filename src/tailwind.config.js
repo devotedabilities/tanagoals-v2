421ce571e3ea1a43e16f5dcf200a799c0c6027ae
devotedabilities <info@devotedabilities.com>
2025-07-01 23:30:28 +1000

    fix: remove duplicate firebase initialization

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -3,6 +3,18 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+  safelist: [
+    'bg-orange-500',
+    'border-orange-600',
+    'text-orange-700',
+    'text-orange-600',
+    'bg-purple-500',
+    'border-purple-600',
+    'text-purple-700',
+    'text-purple-600',
+    'text-red-500',
+    'border-red-400',
+  ],
   theme: {
     extend: {},
   },
